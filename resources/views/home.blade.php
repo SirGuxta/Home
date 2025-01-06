618d6c28ad4a1604495fcba5560760cc7c5c88f9
SirGuxta <96639033+SirGuxta@users.noreply.github.com>
2025-01-06 17:59:03 -0300

    meu primeiro commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,27 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="/css/home.css">
+    <title>Teste</title>
+</head>
+<body>
+    <header>
+        <section class="sec_00">
+            <img src="./img/logov1.png" class="logo">
+            <span>Sir's Lab's</span>
+        </section>
+    </header>
+
+    <main>
+
+    </main>
+
+    <footer>
+
+    </footer>
+</body>
+</html>
